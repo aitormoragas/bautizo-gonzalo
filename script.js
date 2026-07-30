ce1edaa4306bf6914b720828cb5dbe78076c4b32
Aitor MORAGAS GARRIDO <aitor.moragas@amadeus.com>
2026-07-30 14:24:23 +0200

    chore: more pics, autoplay videos + re-ordering

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 const gallery = [
-    { type: "image", src: "images/002.jpg" },
+    { type: "image", src: "images/002(1).jpg" },
     { type: "image", src: "images/0020.jpg" },	
     { type: "image", src: "images/001.jpg" },
     { type: "image", src: "images/003.jpg" },
-    { type: "image", src: "images/005.jpg" },
     { type: "video", src: "videos/002.mp4" },
-    { type: "image", src: "images/004.jpg" },
+    { type: "image", src: "images/016.jpg" },
+    { type: "image", src: "images/005.jpg" },
+    { type: "video", src: "videos/004.mp4" },
+	
+// end of gallery 1
+    { type: "image", src: "images/015.jpg" },
     { type: "image", src: "images/007.jpg" },
-    { type: "video", src: "videos/004.mp4" },	
+    { type: "image", src: "images/004.jpg" },	
+    { type: "image", src: "images/012.jpg" },
+    { type: "image", src: "images/013.jpg" },
+    { type: "image", src: "images/014.jpg" },
+    { type: "image", src: "images/017.jpg" },
+    { type: "image", src: "images/018.jpg" }
+//end of gallery 2
 ];
 
 const mid = Math.ceil(gallery.length / 2);
 
 const lb = document.getElementById("lightbox");
 const lbimg = document.getElementById("lbimg");
+
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        const video = entry.target;
+
+        if (entry.isIntersecting) {
+            video.play().catch(() => {});
+        } else {
+            video.pause();
+            video.currentTime = 0;
+        }
+
+    });
+
+}, {
+    threshold: 0.5
+});
 
 function fill(id, list) {
 
@@ -35,12 +65,26 @@ function fill(id, list) {
             };
 
         } else if (item.type === "video") {
-
-            element = document.createElement("video");
+			
+			element = document.createElement("video");
             element.src = item.src;
-            element.controls = true;
-            element.preload = "metadata";
-            element.playsInline = true;
+			element.muted = true;
+			element.autoplay = true;
+			element.loop = true;
+			element.playsInline = true;
+			element.preload = "metadata";
+			element.controls = true;
+			element.setAttribute("playsinline", "");
+			
+			element.addEventListener("play", () => {
+                element.classList.add("playing");
+            });
+
+            element.addEventListener("pause", () => {
+               element.classList.remove("playing");
+            });
+
+            observer.observe(element);
 
         }
 
